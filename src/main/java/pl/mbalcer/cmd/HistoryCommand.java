23249314767781f15e6374bec8046383d64d55b8
mbalcer <mateusz75319@gmail.com>
2021-09-29 22:26:21 +0200

    Added descriptions and default values to history command

--- src/main/java/pl/mbalcer/cmd/HistoryCommand.java
+++ src/main/java/pl/mbalcer/cmd/HistoryCommand.java
@@ -12,16 +12,25 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 @Dependent
-@Command(name = "history", mixinStandardHelpOptions = true)
+@Command(name = "history",
+        mixinStandardHelpOptions = true,
+        sortOptions = false,
+        version = "Work Time Manager 1.0",
+        header = "Save historical work time",
+        description = "You can save the working time from another day",
+        headerHeading = "Usage:%n%n",
+        synopsisHeading = "%n",
+        descriptionHeading = "%nDescription:%n",
+        optionListHeading = "%nOptions:%n")
 public class HistoryCommand implements Runnable {
 
-    @Option(names = {"--date", "-d"}, required = true)
+    @Option(names = {"--date", "-d"}, required = true, description = "Date in format dd.MM.yyyy")
     String date;
 
-    @Option(names = {"--start", "-s"})
+    @Option(names = {"--start", "-s"}, defaultValue = "8:00", description = "Start working time in format HH:mm")
     String start;
 
-    @Option(names = {"--end", "-e"})
+    @Option(names = {"--end", "-e"}, defaultValue = "16:00", description = "End working time in format HH:mm")
     String end;
 
     @Transactional
