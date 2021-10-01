a5ff06b9445b91c1311c6446f1789e58629d28ff
mbalcer <mateusz75319@gmail.com>
2021-10-01 22:00:36 +0200

    Added counted working time for the day in the print subcommand

--- src/main/java/pl/mbalcer/cmd/PrintCommand.java
+++ src/main/java/pl/mbalcer/cmd/PrintCommand.java
@@ -8,6 +8,7 @@ import pl.mbalcer.model.WorkTime;
 import javax.enterprise.context.Dependent;
 import javax.transaction.Transactional;
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 @Dependent
@@ -26,11 +27,15 @@ public class PrintCommand implements Runnable {
         }
         List<WorkTime> allByMonth = WorkTime.findAllByMonth(MonthYear.findByMonthAndYear(month, today.getYear()));
         allByMonth.forEach(workTime -> {
-            String format = String.format("%d.%d %s - %s",
+            long minutes = ChronoUnit.MINUTES.between(workTime.getStartTime(), workTime.getEndTime());
+            long hours = ChronoUnit.HOURS.between(workTime.getStartTime(), workTime.getEndTime());
+
+            String format = String.format("%02d.%02d %s - %s (%02d:%02d)",
                     workTime.getDayOfMonth(),
                     workTime.getMonthYear().getMonth(),
                     workTime.getStartTime().toString(),
-                    workTime.getEndTime().toString());
+                    workTime.getEndTime().toString(),
+                    hours, minutes - (hours * 60));
             System.out.println(format);
         });
     }
