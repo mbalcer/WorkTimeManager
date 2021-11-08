872c096dbe0e0ed6f7b3ef913f725a1c7a1a7e49
mbalcer <mateusz75319@gmail.com>
2021-11-08 21:29:31 +0100

    Added breaks in the day to the report

--- src/main/java/pl/mbalcer/repository/BreakTimeRepository.java
+++ src/main/java/pl/mbalcer/repository/BreakTimeRepository.java
@@ -2,9 +2,14 @@ package pl.mbalcer.repository;
 
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import pl.mbalcer.model.BreakTime;
+import pl.mbalcer.model.WorkTime;
 
 import javax.enterprise.context.ApplicationScoped;
+import java.util.List;
 
 @ApplicationScoped
 public class BreakTimeRepository implements PanacheRepository<BreakTime> {
+    public List<BreakTime> findByWorkTime(WorkTime workTime) {
+        return find("workTime", workTime).list();
+    }
 }
--- src/main/java/pl/mbalcer/service/impl/ReportServiceImpl.java
+++ src/main/java/pl/mbalcer/service/impl/ReportServiceImpl.java
@@ -1,7 +1,9 @@
 package pl.mbalcer.service.impl;
 
+import pl.mbalcer.model.BreakTime;
 import pl.mbalcer.model.MonthYear;
 import pl.mbalcer.model.WorkTime;
+import pl.mbalcer.repository.BreakTimeRepository;
 import pl.mbalcer.repository.WorkTimeRepository;
 import pl.mbalcer.service.ReportService;
 import pl.mbalcer.util.DateConstant;
@@ -16,9 +18,13 @@ import java.util.stream.Collectors;
 @ApplicationScoped
 public class ReportServiceImpl implements ReportService {
     private static final String DAY_REPORT_FORMAT = "%02d.%02d %s - %s [%02d:%02d]";
+    private static final String ONE_BREAK_REPORT_FORMAT = "%s - %s";
 
     @Inject
     WorkTimeRepository workTimeRepository;
+
+    @Inject
+    BreakTimeRepository breakTimeRepository;
 
     @Override
     public String createMonthlyWorkTimeReport(MonthYear monthYear) {
@@ -37,10 +43,26 @@ public class ReportServiceImpl implements ReportService {
                             workTime.getMonthYear().getMonth(),
                             (workTime.getStartTime() != null) ? workTime.getStartTime().format(DateConstant.REPORT_TIME_FORMATTER) : DateConstant.REPORT_TIME_DEFAULT,
                             (workTime.getEndTime() != null) ? workTime.getEndTime().format(DateConstant.REPORT_TIME_FORMATTER) : DateConstant.REPORT_TIME_DEFAULT,
-                            hours, minutes - (hours * 60));
+                            hours, minutes - (hours * 60))
+                            + getBreaksInDayReport(workTime);
                 })
                 .collect(Collectors.joining("\n"));
 
         return workTimeReport;
+    }
+
+    private String getBreaksInDayReport(WorkTime workTime) {
+        StringBuilder breaksInDayReport = new StringBuilder();
+        List<BreakTime> breaks = breakTimeRepository.findByWorkTime(workTime);
+        if (breaks.size() > 0) {
+            breaksInDayReport.append(" (");
+            breaksInDayReport.append(breaks.stream()
+                    .map(breakTime -> String.format(ONE_BREAK_REPORT_FORMAT,
+                                breakTime.getStartBreak().format(DateConstant.REPORT_TIME_FORMATTER),
+                                breakTime.getEndBreak().format(DateConstant.REPORT_TIME_FORMATTER)))
+                    .collect(Collectors.joining(", ")));
+            breaksInDayReport.append(")");
+        }
+        return breaksInDayReport.toString();
     }
 }
